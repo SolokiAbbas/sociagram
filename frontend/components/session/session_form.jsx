29d4343df61f42f6ec7c3032283024981507cd12
Abbas <SolokiAbbas@gmail.com>
2017-08-22 10:47:45 -0400

    added correct form types

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -9,44 +9,73 @@ class SessionForm extends React.Component {
     super(props);
     this.state = {
       username: "",
-      password: ""
+      password: "",
+      Fname: "",
+      Lname: "",
+      email: "",
+      handle: "@",
     };
-    this.handleUsernameInput = this.handleUsernameInput.bind(this);
-    this.handlePasswordInput = this.handlePasswordInput.bind(this);
+    this.update = this.update.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(e) {
     e.preventDefault();
-    const user = Object.assign({}, this.state);
+    let nextState = this.state;
+    nextState.handle="@"+this.state.username;
+    const user = Object.assign({}, nextState );
     this.props.processForm(user);
-    this.setState({username: '', password: ''});
+    this.setState({username: '', password: '', handle: '@', Fname: '', Lname: '', email: ''});
   }
 
-  handleUsernameInput(e) {
-    this.setState({username: e.currentTarget.value});
+  update(field) {
+    return e => this.setState({[field]: e.currentTarget.value});
   }
 
-  handlePasswordInput(e) {
-    this.setState({password: e.currentTarget.value});
-  }
 
   render(){
+    if(this.props.formType === 'login' && !this.props.loggedIn){
+      return(
+        <div>
+          <h2>Sociagram</h2>
+          <form className="signup-form" onSubmit={this.handleSubmit}>
+            <input placeholder="Username" onChange={this.update('username')} value={this.state.username} />
+            <br />
+            <input placeholder="Password" onChange={this.update('password')} value={this.state.password} />
+              <br />
+            <input type="submit" value="Log in" />
+          </form>
+          <p> {this.props.errors.join(", ")} </p>
+          <div className="signup-button">
+            <Link to="/signup">
+              Sign Up
+            </Link>
+          </div>
+        </div>
+      );
+    }
     return (
       <div>
         {this.props.loggedIn ? <Redirect to="/" /> : <div></div> }
-        <h2>{this.props.formType === 'signup' ? "Sign Up" : "Log In"}</h2>
-        <form onSubmit={this.handleSubmit}>
-          Username <input onChange={this.handleUsernameInput} value={this.state.username} />
+        <h2>Sociagram</h2>
+        <form className="signup-form" onSubmit={this.handleSubmit}>
+          <input placeholder="Email" onChange={this.update('email')} value={this.state.email} />
             <br />
-          Password <input onChange={this.handlePasswordInput} value={this.state.password} />
+          <input placeholder="First Name" onChange={this.update('Fname')} value={this.state.Fname} />
             <br />
-          <input type="submit" value="Submit" />
+          <input placeholder="Last Name" onChange={this.update('Lname')} value={this.state.Lname} />
+            <br />
+          <input placeholder="Username" onChange={this.update('username')} value={this.state.username} />
+          <br />
+          <input placeholder="Password" onChange={this.update('password')} value={this.state.password} />
+            <br />
+          <input type="submit" value="Sign up" />
         </form>
-        <p> {this.props.errors} </p>
-        <Link to={this.props.formType === 'signup' ? "/login" : "/signup"}>
-          {this.props.formType === 'signup' ? "Log In" : "Sign Up"}
-        </Link>
+        <p> {this.props.errors.join(", ")} </p>
+        <p className="error-login">By signing up, you agree to our Terms & Privacy Policy.</p>
+          <div className="login-button">
+            Have an account? <Link to="/login">Log in</Link>
+          </div>
       </div>
     );
   }
