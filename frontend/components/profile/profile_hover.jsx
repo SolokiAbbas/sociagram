11c6b5c0b25a3322a8f45e34824e6a535ecdf943
Abbas <SolokiAbbas@gmail.com>
2018-03-19 23:32:59 -0400

    added bootstrap

--- frontend/components/profile/profile_hover.jsx
+++ frontend/components/profile/profile_hover.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const profileHover = (props) => {
   return(
     <div>
-      
+      <div className="ion-ios-heart-outline">{props.likes}</div>
+      <div className="ion-ios-chatbubble-outline">{props.comments}</div>
     </div>
   );
 };
