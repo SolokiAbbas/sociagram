9bf91bcf4d417ccf345d0e2e77b13ee5210a4074
Abbas <SolokiAbbas@gmail.com>
2018-03-20 21:56:41 -0400

    fixed css

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -9,12 +9,12 @@ class NavBar extends React.Component{
     if(typeof this.props.session !== 'undefined'){
     return(
       <div className="parent-nav">
-        <ul className="navbar">
+        <ul className="navbar2">
           <div className="logo-container">
             <Link to="/"><img className="logo-image" src={window.staticImage.sociagramlogo} alt="Sociagram"></img></Link>
           </div>
           <input className="navbar-search" type="text" placeholder="Search"></input>
-          <div className="top-right-nav">
+          <div className="top-right-nav2">
             <Link to="/new"><img className="profile-image" src={'https://s3.amazonaws.com/sociagram-dev/posts/icons/add.png'}/></Link>
             <Link to="/profile"><img className="profile-image" src={'https://s3.amazonaws.com/sociagram-dev/posts/icons/avatar.png'}/></Link>
           </div>
