569da5d1359a70c298950a79ff0b929506ae9e42
Abbas <SolokiAbbas@gmail.com>
2018-03-22 15:17:41 -0400

    added new modal

--- frontend/components/profile/profile_modal.jsx
+++ frontend/components/profile/profile_modal.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import CommentsFormContainer from '../comments/comments_form_container';
+import CommentsItemContainer from '../comments/comments_item_container';
 
 class ProfileHover extends React.Component{
   render(){
+    let author = this.props.post.author_id;
     return(
-      <div>
-        
+      <div className="container">
+        <img src={this.props.post.image_url} alt={this.props.post.title} className="post-images"/>
+        <img className="user-avatar" src={this.props.users[author].image_url}/>
+        <div className="comments-part">
+          <CommentsItemContainer post={ this.props.post } />
+          <CommentsFormContainer post={ this.props.post } focus={this.state.input} />
+        </div>
     </div>
   );
   }
