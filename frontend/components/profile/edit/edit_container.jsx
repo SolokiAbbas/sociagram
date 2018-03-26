272bb65f2e653a582dea6711083abcd2f8bf57a8
Abbas <SolokiAbbas@gmail.com>
2018-03-26 00:56:02 -0400

    added container

--- frontend/components/profile/edit/edit_container.jsx
+++ frontend/components/profile/edit/edit_container.jsx
@@ -4,11 +4,15 @@ import { updateUser } from '../../actions/users/users_action';
 import EditItem from './edit_item';
 
 const mapStateToProps = (state) =>{
-
+  return {
+    session: state.session,
+  };
 };
 
 const mapDispatchToProps = (dispatch) => {
-
+  return {
+    updateUser: (user) => dispatch(updateUser(user)),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(EditItem);
