c493e478b1effcc8ab847004ff8bc5be2be8351d
Abbas <SolokiAbbas@gmail.com>
2018-03-26 01:05:05 -0400

    added edit item

--- frontend/components/profile/edit/edit_item.jsx
+++ frontend/components/profile/edit/edit_item.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 class EditItem extends React.Component{
+  render(){
+    let { session, users } = this.props;
+    if(typeof session !== 'undefined'){
+    return(
+        <div className="posts-index">
 
+        </div>
+    );
+  }
+  return(<div></div>);
+  }
 }
 
 export default EditItem;
