dc9be9ad9b80fe44d69777df5e5e15b5b800e4e4
Abbas <SolokiAbbas@gmail.com>
2018-03-26 00:53:22 -0400

    added new files for edit

--- frontend/components/profile/edit/edit_container.jsx
+++ frontend/components/profile/edit/edit_container.jsx
@@ -1,3 +1,14 @@
 import { connect } from 'react-redux';
 import React from 'react';
 import { updateUser } from '../../actions/users/users_action';
+import EditItem from './edit_item';
+
+const mapStateToProps = (state) =>{
+
+};
+
+const mapDispatchToProps = (dispatch) => {
+
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditItem);
