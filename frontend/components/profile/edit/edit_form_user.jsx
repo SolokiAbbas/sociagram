47470be7f84fe1bff2707abe8343dcfc7bdf2828
Abbas <SolokiAbbas@gmail.com>
2018-03-26 12:11:00 -0400

    added form edit

--- frontend/components/profile/edit/edit_form_user.jsx
+++ frontend/components/profile/edit/edit_form_user.jsx
@@ -1,17 +1,43 @@
 import React from 'react';
 
 class EditFormUser extends React.Component{
-  constructor(){
+  constructor(props){
+    super(props);
+    this.state={
+      Fname: this.session.currentUser.Fname,
+      Lname: this.session.currentUser.Lname,
+      email: this.session.currentUser.email,
+      username: this.session.currentUser.username,
+    };
+    this.update = this.update.bind(this);
+    this.update = this.update.bind(this);
+  }
 
+  handleSubmit(e){
+    e.preventDefault();
+    
+  }
+
+  update(field) {
+    return e => this.setState({ [field]: e.currentTarget.value });
   }
 
   render(){
     let { session, users } = this.props;
     if(typeof session !== 'undefined'){
     return(
-        <div className="container">
+      <form className="posts-form" onSubmit={this.handleSubmit}>
+        <label>First Name:</label>
+        <input className="input-field" onChange={this.update('Fname')} value={this.state.Fname} />
+        <label>Last Name:</label>
+        <input className="input-field" onChange={this.update('Lname')} value={this.state.Lname} />
+        <label>Username:</label>
+        <input className="input-field" onChange={this.update('username')} value={this.state.username}/>
+        <label>Email:</label>
+        <input className="input-field" onChange={this.update('email')} value={this.state.email}/>
 
-        </div>
+      <input className="submit-button" type="submit" value="Make Changes" />
+      </form>
     );
   }
   return(<div></div>);
