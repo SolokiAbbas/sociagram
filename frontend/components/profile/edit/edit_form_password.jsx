2659b70a75b86a9099cd4777fd327b98f032674e
Abbas <SolokiAbbas@gmail.com>
2018-03-28 10:52:23 -0400

    added password field

--- frontend/components/profile/edit/edit_form_password.jsx
+++ frontend/components/profile/edit/edit_form_password.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 
 class EditFormPassword extends React.Component{
+  constructor(props){
+    super(props);
+    this.current_id = this.props.users.id;
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("user[password]", this.state.password);
+    formData.append("user[newpassword]", this.state.Fname);
+    this.props.updateUser(formData, this.current_id);
+  }
 
   render(){
     let { users } = this.props;
     if(typeof users !== 'undefined'){
     return(
-        <div className="container">
+      <form className="user-edit-form" onSubmit={this.handleSubmit}>
 
-        </div>
+      </form>
     );
   }
   return(<div></div>);
