734a02f99ac2fd5fb6c9366f0fc9b6108b0c2b4b
Abbas <SolokiAbbas@gmail.com>
2018-04-26 20:55:49 -0400

    added action to container

--- frontend/components/posts/posts_container.jsx
+++ frontend/components/posts/posts_container.jsx
@@ -2,6 +2,7 @@ import { connect } from 'react-redux';
 import React from 'react';
 import { fetchAllPosts, fetchAPost, deleteAPost } from '../../actions/posts/posts_action';
 import { createALike, deleteALike } from '../../actions/likes/likes_action';
+import { createABookmark, deleteABookmark } from '../../actions/bookmarks/bookmarks_action';
 import PostsIndex from './posts_index';
 import { fetchAUser } from '../../actions/users/users_action';
 import PostsIndexItem from './posts_index_item';
@@ -22,6 +23,8 @@ const mapDispatchToProps = (dispatch) => {
     deleteAPost: (id) => dispatch(deleteAPost(id)),
     createALike: (like) => dispatch(createALike(like)),
     deleteALike: (id) => dispatch(deleteALike(id)),
+    createABookmark: (bookmark) => dispatch(createABookmark(bookmark)),
+    deleteABookmark: (id) => dispatch(deleteABookmark(id)),
   };
 };
 
