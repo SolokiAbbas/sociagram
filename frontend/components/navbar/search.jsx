f3a17d0278c4449d3cc7e695ce754fb63c426eab
Abbas <SolokiAbbas@gmail.com>
2018-05-02 00:51:17 -0400

    added constructor

--- frontend/components/navbar/search.jsx
+++ frontend/components/navbar/search.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import searchUsers from '../../../util/users/users_api_util';
 
 class Search extends React.Component{
-  
+  constructor(props){
+    super(props);
+    this.state = {
+      query: ''
+    };
+  }
+
+  render(){
+    return(
+      <div>
+      </div>
+    );
+  }
 }
 
 export default Search;
