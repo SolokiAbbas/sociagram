431f86bdd236bc5c7820e4b6cdcae625af761616
Abbas <SolokiAbbas@gmail.com>
2018-05-03 01:08:24 -0400

    added seggustions to props

--- frontend/components/navbar/suggestions.jsx
+++ frontend/components/navbar/suggestions.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Suggestions = (props) => {
-
+  const options = props.results.map(res => (
+    <li key={res.id}>
+      @{res.username}
+    </li>
+  ));
+  return <ul>{options}</ul>;
 };
 
-export default Suggestions; 
+export default Suggestions;
