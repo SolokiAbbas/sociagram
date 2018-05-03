80844a36e2a37d46d037f75ba8a8dfe702753395
Abbas <SolokiAbbas@gmail.com>
2018-05-03 01:10:42 -0400

    added suggestion links

--- frontend/components/navbar/suggestions.jsx
+++ frontend/components/navbar/suggestions.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Suggestions = (props) => {
   const options = props.results.map(res => (
     <li key={res.id}>
-      @{res.username}
+      <Link to={`/profile/${res.id}`}>@{res.username}</Link>
     </li>
   ));
   return <ul>{options}</ul>;
