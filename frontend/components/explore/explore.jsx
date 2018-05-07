02477a0a6a9d518e8d8f6223449b3edc13170226
Abbas <SolokiAbbas@gmail.com>
2018-05-07 02:36:09 -0400

    added users to explore

--- frontend/components/explore/explore.jsx
+++ frontend/components/explore/explore.jsx
@@ -8,7 +8,6 @@ class Explore extends React.Component{
   }
 
   selectRandom(){
-
     let randomNum1 = Math.floor(Math.random()*this.props.users.length);
     let randomNum2 = Math.floor(Math.random()*this.props.users.length);
     let randomNum3 = Math.floor(Math.random()*this.props.users.length);
@@ -17,10 +16,9 @@ class Explore extends React.Component{
       randomNum2 = Math.floor(Math.random()*this.props.users.length);
       randomNum3 = Math.floor(Math.random()*this.props.users.length);
     }
-
-  this.randomUsers.push(this.props.users[randomNum1]);
-  this.randomUsers.push(this.props.users[randomNum2]);
-  this.randomUsers.push(this.props.users[randomNum3]);
+    this.randomUsers.push(this.props.users[randomNum1]);
+    this.randomUsers.push(this.props.users[randomNum2]);
+    this.randomUsers.push(this.props.users[randomNum3]);
   }
 
   render(){
@@ -29,7 +27,11 @@ class Explore extends React.Component{
       <div>
         <div>
           <li>Discover People</li>
-          <ExploreUser user={this.props.user} createAFollow={this.props.createAFollow}
+          <ExploreUser user={this.randomUsers[0]} createAFollow={this.props.createAFollow}
+            deleteAFollow={this.props.deleteAFollow} />
+          <ExploreUser user={this.randomUsers[1]} createAFollow={this.props.createAFollow}
+            deleteAFollow={this.props.deleteAFollow} />
+          <ExploreUser user={this.randomUsers[2]} createAFollow={this.props.createAFollow}
             deleteAFollow={this.props.deleteAFollow} />
         </div>
         <div>
