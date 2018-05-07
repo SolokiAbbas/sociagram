8e132b12112ec13db32bd43be23cb01f0b801f01
Abbas <SolokiAbbas@gmail.com>
2018-05-07 02:10:35 -0400

    added random users

--- frontend/components/explore/explore.jsx
+++ frontend/components/explore/explore.jsx
@@ -2,13 +2,25 @@ import React from 'react';
 import ExploreUser from './explore_user';
 
 class Explore extends React.Component{
+  constructor(){
+    this.randomUsers = []
+  }
+
+  selectRandom(){
+
+    
+  }
 
   render(){
+    let randomUser1 = this.props.users[Math.floor(Math.random()*this.props.users.length)];
+    let randomUser2 = this.props.users[Math.floor(Math.random()*this.props.users.length)];
+    let randomUser3 = this.props.users[Math.floor(Math.random()*this.props.users.length)];
     return(
       <div>
         <div>
           <li>Discover People</li>
-          <ExploreUser />
+          <ExploreUser user={this.props.user} createAFollow={this.props.createAFollow}
+            deleteAFollow={this.props.deleteAFollow} />
         </div>
         <div>
           <li>Explore</li>
