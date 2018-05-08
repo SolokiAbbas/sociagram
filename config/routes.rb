4bb983f8d4dc3de50e6b1410aeb29a0a2bf0000b
Abbas <SolokiAbbas@gmail.com>
2018-05-08 00:55:36 -0400

    added all users in routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'static_pages#root'
 
   namespace :api, defaults: { format: :json } do
-    resources :users, only: [:create, :show, :update, :edit] do
+    resources :users, only: [:create, :show, :update, :edit, :index] do
       resources :follows, only: [ :index ]
     end
     post 'search', to: 'users#search'
